144c14c2e7b0533aff02ac0799f97de0056df40d
tinorodram <tinorodram@gmail.com>
2026-09-11 14:23:27 +0200

    chore: remove tracked logs now ignored by gitignore

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-ml qmio/hpc gcccore/12.3.0 python/3.11.9
+ml qmio/hpc gcc/12.3.0 qmio-qiskitcpp/896f9fa-python-3.11.9
 
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ STEPS=${2:-15}
 
 ml qmio/hpc  gcccore/12.3.0 python/3.11.9
  
-./build/bell_circuit "$SHOTS" "$STEPS"
+./build/basic_circuit
  
 
  
